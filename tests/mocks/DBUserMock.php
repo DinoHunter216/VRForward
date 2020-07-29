17e29b30d212c0e8237d912b853ef8112de52b89
DinoHunter216 <jeremydumas2002@gmail.com>
2020-07-29 15:56:33 -0400

    Small mock change

--- tests/mocks/DBUserMock.php
+++ tests/mocks/DBUserMock.php
@@ -30,12 +30,4 @@ class DBUserMock
     {
         return true;
     }
-
-    public function all()
-    {
-        return [
-                ['1', 'username', 'firstName', 'lastName', 'email', 'password', 'type'],
-                ['2', 'username2', 'firstName2', 'lastName2', 'email2', 'password2', 'type2']
-               ];
-    }
 }
